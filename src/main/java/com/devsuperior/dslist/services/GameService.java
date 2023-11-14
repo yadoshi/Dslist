16207bb7cd009464ab171484349584b26811eafe
yadoshi <yagodfernandes@gmail.com>
2023-11-14 13:58:14 -0300

    game dto,service, controller, repository

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        var all = gameRepository.findAll();
+        return all.stream().map(GameMinDTO::new).toList();
+    }
+}
